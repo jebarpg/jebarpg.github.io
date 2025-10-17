1d217c9e9a8e04034e53c4bb2e46bdd1be02d52d
jebarpg <jebarpg@gmail.com>
2025-10-17 09:08:18 -0700

    updated hello world!

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <h1>Hello World!</h1>
+      <a href="enemyGenerator/index.html" target="_blank" rel="noopener noreferrer">Enemy Generator</a>
     </>
   )
 }
