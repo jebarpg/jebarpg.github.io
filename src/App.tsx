27d0fa9eaa755e05a20587f515f9afcef1f5d2b2
jebarpg <jebarpg@gmail.com>
2025-10-17 09:51:48 -0700

    updated Enemy Generator Link

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1>Hello World!</h1>
-      <a href="enemyGenerator/index.html" target="_blank" rel="noopener noreferrer">Enemy Generator</a>
+      <a href="enemyGenerator/" target="_blank" rel="noopener noreferrer">Enemy Generator</a>
     </>
   )
 }
